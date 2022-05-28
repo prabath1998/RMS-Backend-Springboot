3c04ed8563addd3eb15c7c09a5cbda2592b18569
prabath1998 <prabhathudayanga1998@gmail.com>
2022-05-28 22:17:45 +0530

    login api

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const connection = require('../connection');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 router.post('/signup', (req, res) => {
     let user = req.body;
@@ -26,4 +29,27 @@ router.post('/signup', (req, res) => {
     });
 
 });
+
+router.post('/login', (req, res) => {
+    const user = req.body;
+    query = "select email,password,role,status from user where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Incorrect username or password." });
+            } else if (results[0].status === 'false') {
+                return res.status(401).json({ message: "Wait for admin approval" });
+            } else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role }
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' });
+                res.status(200).json({ token: accessToken });
+            } else {
+                return res.status(400).json({ message: "Something went wrong..!. Please try again later." });
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+});
+
 module.exports = router;
