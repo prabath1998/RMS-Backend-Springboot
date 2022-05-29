f2c73975ba152b1c82e671cc1422ee8ffa4514d0
prabath1998 <prabhathudayanga1998@gmail.com>
2022-05-29 12:59:24 +0530

    update category api

--- routes/category.js
+++ routes/category.js
@@ -17,11 +17,28 @@ router.post('/add', auth.authenticateToken, checkRole.checkRole, (req, res, next
     });
 });
 
+//get categories api
 router.get('/get', auth.authenticateToken, (req, res, next) => {
     var query = "select * from category order by name";
     connection.query(query, (err, results) => {
         if (!err) {
             return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+//update category api
+router.patch('/update', auth.authenticateToken, checkRole.checkRole, (req, res, next) => {
+    let category = req.body;
+    var query = "update category set name=? where id=?";
+    connection.query(query, [category.name, category.id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "Category id does not found." });
+            }
+            return res.status(200).json({ message: "Category updated successfully." })
         } else {
             return res.status(500).json(err);
         }
