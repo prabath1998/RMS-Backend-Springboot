6a88ed83ca0e195a4a7960243d644818334eb2c8
prabath1998 <prabhathudayanga1998@gmail.com>
2022-05-29 22:52:49 +0530

    product api's added

--- routes/product.js
+++ routes/product.js
@@ -49,9 +49,43 @@ router.get('/getById/:id', auth.authenticateToken, (req, res, next) => {
     var query = "select id,name,description,price from product where id=?";
     connection.query(query, [id], (err, results) => {
         if (!err) {
-            return res.status(200).json(results);
+            return res.status(200).json(results[0]);
         } else {
             return res.status(500).json(err);
+        }
+    });
+});
+
+//update product
+router.patch('/update', auth.authenticateToken, checkRole.checkRole, (req, res, next) => {
+    let product = req.body;
+    var query = "update product set name=?, categoryId=?, description=?, price=? where id=?";
+    connection.query(query, [product.name, product.categoryId, product.description, product.price, product.id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "Product id does not exists." })
+            } else {
+                return res.status(200).json({ message: "Product updated successfully" });
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+//delete product api
+router.delete('/delete/:id', auth.authenticateToken, checkRole.checkRole, (req, res, next) => {
+    const id = req.params.id;
+    var query = "delete from product where id=?";
+    connection.query(query, [id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "Product id does not found" });
+            } else {
+                return res.status(200).json({ message: "Product is deleted successfully.." });
+            }
+        } else {
+            return res.res.status(500).json(err);
         }
     });
 });
