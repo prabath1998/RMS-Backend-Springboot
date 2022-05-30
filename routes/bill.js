f34ce6cf8a976fd954523f7eb06506708b3592c7
prabath1998 <prabhathudayanga1998@gmail.com>
2022-05-30 15:56:10 +0530

    getPdf api added

--- routes/bill.js
+++ routes/bill.js
@@ -36,4 +36,30 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
     });
 });
 
+router.post('/getPdf', auth.authenticateToken, function(req, res) {
+    const orderDetails = req.body;
+    const pdfPath = './generated_pdf/' + orderDetails.uuid + '.pdf';
+    if (fs.existsSync(pdfPath)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(pdfPath).pipe(res);
+    } else {
+        var productDetailsReport = JSON.parse(orderDetails.productDetails);
+        ejs.renderFile(path.join(__dirname, '', "report.ejs"), { productDetails: productDetailsReport, name: orderDetails.name, email: orderDetails.email, contactNumber: orderDetails.contactNumber, paymentMethod: orderDetails.paymentMethod, totalAmount: orderDetails.totalAmount }, (err, results) => {
+            if (err) {
+                return res.status(500).json(err);
+            } else {
+                pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf", function(err, data) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).json(err);
+                    } else {
+                        res.contentType("application/pdf");
+                        fs.createReadStream(pdfPath).pipe(res);
+                    }
+                });
+            }
+        });
+    }
+});
+
 module.exports = router;
