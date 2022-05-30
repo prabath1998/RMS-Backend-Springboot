63e5864132252fca645d65fb8d2d13cffef885b8
prabath1998 <prabhathudayanga1998@gmail.com>
2022-05-30 20:06:50 +0530

    get all bills api added

--- routes/bill.js
+++ routes/bill.js
@@ -62,4 +62,16 @@ router.post('/getPdf', auth.authenticateToken, function(req, res) {
     }
 });
 
+//get all the bills
+router.get('/getBills', auth.authenticateToken, (req, res, next) => {
+    var query = "select * from bill order by id DESC";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
 module.exports = router;
